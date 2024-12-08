13f5db9a5f0ca085fdf112bfb6ef872dcea34725
Lucas Otoni <lucasotoni.dev@gmail.com>
2024-12-08 18:40:47 -0300

    feat: add next to middleware

--- Back-end/middleware/errorHandler.js
+++ Back-end/middleware/errorHandler.js
@@ -1,14 +1,13 @@
 const AppError = require('../utils/errorUtil');
 
-const errorHandler = (err, req, res) => {
-  if (err instanceof AppError) {
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {  
+  if (err instanceof AppError) {    
     return res.status(err.statusCode).json({
       status: 'error',
       message: err.message,
     });
   }
-  
-  console.log(err);
 
   return res.status(500).json({
     status: 'error',
