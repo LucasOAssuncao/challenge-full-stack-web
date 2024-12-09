8f2d3bb00533f9fdc9ec6d9ce96db985cd38f0ae
Lucas Otoni <lucasotoni.dev@gmail.com>
2024-12-09 19:18:49 -0300

    feat: jest + lint config

--- Back-end/eslint.config.mjs
+++ Back-end/eslint.config.mjs
@@ -1,10 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    plugins: ["jest"],
+    extends: ["plugin:jest/recommended"],
+  },
 ];
