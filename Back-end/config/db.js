8944f68b2b4cc10852e0faf70e872c818500bfc9
Lucas Otoni <lucasotoni.dev@gmail.com>
2024-12-09 19:18:31 -0300

    feat: add charset at config

--- Back-end/config/db.js
+++ Back-end/config/db.js
@@ -1,19 +1,20 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  charset: "utf8mb4",
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
+    console.error("Erro ao conectar ao banco de dados:", err);
     return;
   }
-  console.log('Conectado ao banco de dados');
+  console.log("Conectado ao banco de dados");
 });
 
 module.exports = connection;
